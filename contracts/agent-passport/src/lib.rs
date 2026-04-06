c881e6031cafb2c3c0fced870622eca4011f1eb2
ggoldani <tech.goldani@gmail.com>
2026-04-06 12:43:43 -0300

    task-15: implement init and config retrieval

--- contracts/agent-passport/src/lib.rs
+++ contracts/agent-passport/src/lib.rs
@@ -1,7 +1,36 @@
 #![no_std]
 
+use soroban_sdk::{contract, contractimpl, Address, Env};
+
+use crate::storage::{read_config, write_config};
+use crate::types::Config;
+
 mod errors;
 mod storage;
-#[cfg(test)]
-mod test;
 mod types;
+
+#[contract]
+pub struct AgentPassport;
+
+#[contractimpl]
+impl AgentPassport {
+    pub fn init(env: Env, admin: Address, authorized_relayer: Address) {
+        let config = Config {
+            admin,
+            authorized_relayer,
+        };
+
+        write_config(&env, &config);
+    }
+
+    pub fn get_config(env: Env) -> Config {
+        read_config(&env).unwrap()
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::{AgentPassport, AgentPassportClient};
+
+    include!("test.rs");
+}
